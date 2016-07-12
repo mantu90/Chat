2a18e18a6194e8c75f19242558d693b091a954fd
root <root@ip-172-31-28-111.eu-central-1.compute.internal>
2016-07-12 10:02:17 +0000

    Changes v0.1

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var port = 80;
-
-
-app.get("/chat",function(req,res){
-        res.send("<h1>Hello from Juuso</h1>");
-});
 
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+
 app.get("/", function(req, res){
     res.render("page");
 });
@@ -21,13 +17,18 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
+    // Join chat
+    console.log('Socket succesfully connected with id: '+socket.id);
+    // Welcome chat text
+    socket.emit('message', { message: 'Welcome to the chat!' });
     console.log('a user connected');
-    
-    socket.on('disconnect', function(){
-      console.log('user disconnected');
-    });
 
-    socket.emit('message', { message: 'Tervetuloa chattiin!' });
+    // Disconnect chat
+    socket.on('disconnect', function(){
+    	console.log('user disconnected');
+    });
+    
+    // Send message
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
--- public/chat.js
+++ public/chat.js
@@ -5,24 +5,39 @@ window.onload = function() {
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
+ 
+// Send chat message    
 
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data.message);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
-            }
-            content.innerHTML = html;
-        } else {
+// add zero to time
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+socket.on('message', function (data) {
+	if(data.message) {
+        // add time to message    
+	var d = new Date(); // for now
+	data.message = addZero(d.getHours())+":"+addZero(d.getMinutes())+":"+addZero(d.getSeconds())+": " + data.message;
+	messages.push(data.message);
+	var html = '';
+	
+	for(var i=0; i<messages.length; i++) {
+		html +=  messages[i] + '<br />';
+        }
+	content.innerHTML = html;
+        
+	} else {
             console.log("There is a problem:", data);
         }
-    });
+});
 
-    sendButton.onclick = function() {
-        var text = field.value;
+sendButton.onclick = function() {
+	var text = field.value;
         socket.emit('send', { message: text });
-	field.empty();
+	field.value = "";
     };
 
 }
